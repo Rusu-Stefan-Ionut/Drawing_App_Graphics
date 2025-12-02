168bf7f316ab8e76a2d006d61a72de88ecf032d3
Rusu-Stefan-Ionut <stefanrusu15@yahoo.com>
2025-12-03 00:10:19 +0200

    Tema-2 added helper functions, refactored code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import numpy as np
 
 from Point2D import Point2D
-from transformare2D import Transformare2D
+from transformare2D import Transform2D
 
 from PyQt5.QtGui import QPainter, QPixmap, QImage, QPen, QColor, QIcon
 from PyQt5.QtCore import Qt, QPoint, QPointF, QRect, QSize, pyqtSignal
@@ -37,8 +37,8 @@ class Canvas(QWidget):
         self.drag_start: QPoint | None = None
         self.original_points: list[Point2D] | None = None
 
-        self.geometric_center: float | None = None
-        self.start_angle: float | None = None
+        self.geometric_center: Point2D | None = None
+        self.start_angle: np.ndarray | None = None
 
         self.setMinimumSize(400, 300)
         self.setFocusPolicy(Qt.StrongFocus)
@@ -72,7 +72,8 @@ class Canvas(QWidget):
                 pos = event.pos()
                 
                 for i, p in enumerate(self.points):
-                    if self._distance(p, pos) <= self.drag_threshold:
+                    mouse_point = Point2D(pos.x(), pos.y())
+                    if self.euclidian_distance(p, mouse_point) <= self.drag_threshold:
                         self.drag_index = i
                         self.setCursor(Qt.ClosedHandCursor)
                         self.update()
@@ -82,21 +83,18 @@ class Canvas(QWidget):
                 self.update()
                 
         elif "transform" == self.mode:
+            self.geometric_center = self.compute_centroid()
+            self.original_points = [Point2D(p.x, p.y) for p in self.points]
+
             if event.button() == Qt.LeftButton:
                 self.drag_start = event.pos()
-                self.original_points = [Point2D(p.x, p.y) for p in self.points]
-                self.geometric_center = self.compute_centroid()
-
-                self.scaling_mode = event.modifiers() & Qt.ShiftModifier
+                
 
             elif event.button() == Qt.RightButton:
+                
                 self.drag_start = event.pos()
-                self.original_points = [Point2D(p.x, p.y) for p in self.points]
-
-                self.geometric_center = self.compute_centroid()
-
-                dx = event.x() - self.geometric_center.x()
-                dy = event.y() - self.geometric_center.y()
+                dx = event.x() - self.geometric_center.x
+                dy = event.y() - self.geometric_center.y
                 self.start_angle = np.arctan2(dy, dx)
             
 
@@ -114,7 +112,9 @@ class Canvas(QWidget):
                 dx = event.x() - self.drag_start.x()
                 dy = event.y() - self.drag_start.y()
 
-                if self.scaling_mode:
+                shift_pressed = QApplication.keyboardModifiers() & Qt.ShiftModifier
+
+                if shift_pressed:
                     dist = (dx**2 + dy**2) ** 0.5
                     factor = 1.0 + (dist / 10.0) * 0.1
 
@@ -122,24 +122,18 @@ class Canvas(QWidget):
                     if dx + dy < 0:
                         factor = 1/factor
 
-                    T = Transformare2D() \
-                        .translatie(-self.geometric_center.x(), -self.geometric_center.y()) \
-                        .scalare(factor, factor) \
-                        .translatie(self.geometric_center.x(), self.geometric_center.y())
-
+                    cx, cy = self.geometric_center.x, self.geometric_center.y
+                    T = Transform2D().scale_about_point(factor, factor, cx, cy)
                     self.apply_transformation(T)
-                    
                     self.update()
 
                 else:
-                    T = Transformare2D().translatie(dx, dy)
-
+                    T = Transform2D().translation(dx, dy)
                     self.apply_transformation(T)
-
                     self.update()
 
             if (event.buttons() & Qt.RightButton) and self.drag_start is not None:
-                cx, cy = self.geometric_center.x(), self.geometric_center.y()
+                cx, cy = self.geometric_center.x, self.geometric_center.y
 
                 # current angle
                 dx = event.x() - cx
@@ -151,11 +145,7 @@ class Canvas(QWidget):
                 cos_a = np.cos(delta_angle)
                 sin_a = np.sin(delta_angle)
 
-                T = Transformare2D() \
-                        .translatie(-cx, -cy) \
-                        .rotatie(cos_a, sin_a) \
-                        .translatie(cx, cy)
-
+                T = Transform2D().rotate_about_point(cos_a, sin_a, cx, cy)
                 self.apply_transformation(T)
 
                 self.update()
@@ -165,6 +155,7 @@ class Canvas(QWidget):
             self.drag_index = None
             self.setCursor(Qt.ArrowCursor)
             self.update()
+        self.start_angle = None
 
     def keyPressEvent(self, event):     
         key = event.key()
@@ -183,7 +174,6 @@ class Canvas(QWidget):
                 self.drag_start = None
                 self.original_points = None
                 self.start_angle = None
-                self.scaling_mode = False
 
                 print("Mode:", self.mode)
                 self.update()
@@ -192,21 +182,21 @@ class Canvas(QWidget):
             case _:
                 super().keyPressEvent(event)
 
-    def _distance(self, p1: Point2D, p2: QPoint) -> float:
-        dx = p1.x - p2.x()
-        dy = p1.y - p2.y()
+    def euclidian_distance(self, p1: Point2D, p2: Point2D) -> float:
+        dx = p1.x - p2.x
+        dy = p1.y - p2.y
         return (dx * dx + dy * dy) ** 0.5
     
-    def compute_centroid(self):
+    def compute_centroid(self) -> Point2D:
         if not self.points:
-            return QPointF(0, 0)
-
+            return Point2D(0, 0)
+        
         x_sum = sum(p.x for p in self.points)
         y_sum = sum(p.y for p in self.points)
         n = len(self.points)
-        return QPointF(x_sum / n, y_sum / n)
+        return Point2D(x_sum / n, y_sum / n)
     
-    def apply_transformation(self, T: Transformare2D):
+    def apply_transformation(self, T: Transform2D):
         self.points = [T.apply_to_point(p) for p in self.original_points]
         self.update()
 
--- transformare2D.py
+++ transformare2D.py
@@ -1,47 +1,54 @@
 import numpy as np
-
 from Point2D import Point2D
 
-class Transformare2D:
+class Transform2D:
     def __init__(self):
         self._matrix = np.eye(3, dtype=float)
 
-    def getMatrix(self):
+    def get_matrix(self):
         return self._matrix
     
-    def _left_transf(self, trasnf_matrix: np.ndarray):
-        self._matrix = trasnf_matrix @ self._matrix
+    def _left_transform(self, transform_matrix: np.ndarray):
+        self._matrix = transform_matrix @ self._matrix
 
-    def translatie(self, dx: float, dy: float):
+    def translation(self, dx: float, dy: float):
         A = np.array([[1, 0, dx],
-                     [0, 1, dy],
-                     [0, 0, 1]],
-                     dtype=float)
-        self._left_transf(A)
+                      [0, 1, dy],
+                      [0, 0, 1]], 
+                      dtype=float)
+        self._left_transform(A)
         return self
     
-    def scalare(self, sx: float, sy: float):
+    def scaling(self, sx: float, sy: float):
         A = np.array([[sx, 0, 0],
                       [0, sy, 0],
-                      [0,  0, 1]], dtype=float)
-        self._left_transf(A)
+                      [0, 0, 1]], 
+                      dtype=float)
+        self._left_transform(A)
         return self
 
-    def rotatie(self, cos_a: float, sin_a: float):
+    def rotation(self, cos_a: float, sin_a: float):
         A = np.array([[ cos_a, -sin_a, 0],
                       [ sin_a,  cos_a, 0],
-                      [     0,      0, 1]], dtype=float)
-        self._left_transf(A)
+                      [0, 0, 1]], 
+                      dtype=float)
+        self._left_transform(A)
         return self
 
-    def simetrie_ox(self):
-        return self.scalare(1, -1)
+    def symmetry_x(self):
+        return self.scaling(1, -1)
 
-    def simetrie_oy(self):
-        return self.scalare(-1, 1)
+    def symmetry_y(self):
+        return self.scaling(-1, 1)
 
-    def simetrie_origine(self):
-        return self.scalare(-1, -1)
+    def symmetry_origin(self):
+        return self.scaling(-1, -1)
+    
+    def scale_about_point(self, sx: float, sy: float, cx: float, cy: float):
+        return self.translation(-cx, -cy).scaling(sx, sy).translation(cx, cy)
+
+    def rotate_about_point(self, cos_a: float, sin_a: float, cx: float, cy: float):
+        return self.translation(-cx, -cy).rotation(cos_a, sin_a).translation(cx, cy)
     
     def apply_to_point(self, point: Point2D) -> Point2D:
         vec = point.toVector()
